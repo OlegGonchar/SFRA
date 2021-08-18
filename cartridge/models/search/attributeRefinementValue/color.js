3cf181cf18a4d844f359d2e076235ae77ccdbf3b
oleg-gonchar <oleg.gonchar@valtech.com>
2021-08-18 11:13:57 +0300

    decreasing the amount of code for color and size js

--- cartridge/models/search/attributeRefinementValue/color.js
+++ cartridge/models/search/attributeRefinementValue/color.js
@@ -1,84 +1,10 @@
-'use strict';
+'use strict'
 
-var BaseAttributeValue = require('*/cartridge/models/search/attributeRefinementValue/base');
+var BaseAttributeValue = module.superModule;
 
-
-/**
- * @constructor
- * @classdesc Color attribute refinement value model
- *
- * @param {dw.catalog.ProductSearchModel} productSearch - ProductSearchModel instance
- * @param {dw.catalog.ProductSearchRefinementDefinition} refinementDefinition - Refinement
- *     definition
- * @param {dw.catalog.ProductSearchRefinementValue} refinementValue - Raw DW refinement value
- */
-function ColorAttributeValue(productSearch, refinementDefinition, refinementValue) {
-    this.productSearch = productSearch;
-    this.refinementDefinition = refinementDefinition;
-    this.refinementValue = refinementValue;
-
-    this.initialize();
-}
-
-ColorAttributeValue.prototype = Object.create(BaseAttributeValue.prototype);
-
-ColorAttributeValue.prototype.initialize = function () {
-    BaseAttributeValue.prototype.initialize.call(this);
-
-    this.type = 'color';
-    this.displayValue = this.getDisplayValue(this.refinementValue);
-    this.swatchId = 'swatch-circle-' + this.presentationId;
-    this.selected = this.isSelected(
-        this.productSearch,
-        this.refinementDefinition.attributeID,
-        this.refinementValue.value
-    );
-    this.url = this.getUrl(
-        this.productSearch,
-        this.actionEndpoint,
-        this.id,
-        this.value,
-        this.selected,
-        this.selectable
-    );
-    this.title = this.getTitle(
-        this.selected,
-        this.selectable,
-        this.refinementDefinition.displayName,
-        this.displayValue
-    );
-};
-
-/**
- * @constructor
- * @classdesc Color attribute refinement value model
- *
- * @param {dw.catalog.ProductSearchModel} productSearch - ProductSearchModel instance
- * @param {dw.catalog.ProductSearchRefinementDefinition} refinementDefinition - Refinement
- *     definition
- * @param {dw.catalog.ProductSearchRefinementValue} refinementValue - Raw DW refinement value
- */
 function ColorRefinementValueWrapper(productSearch, refinementDefinition, refinementValue) {
-    var value = new ColorAttributeValue(
-        productSearch,
-        refinementDefinition,
-        refinementValue
-    );
-    var items = [
-        'id',
-        'type',
-        'displayValue',
-        'presentationId',
-        'selected',
-        'selectable',
-        'swatchId',
-        'title',
-        'url',
-        'hitCount'
-    ];
-    items.forEach(function (item) {
-        this[item] = value[item];
-    }, this);
+    BaseAttributeValue.call(this, productSearch, refinementDefinition, refinementValue)
+    this.hitCount = refinementValue.hitCount;
 }
 
 module.exports = ColorRefinementValueWrapper;
